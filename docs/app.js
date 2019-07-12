5e383fbe7af439f4877336be2a4e4c20cf39baad
niawjunior <pasupol@dome.cloud>
2019-07-12 20:33:44 +0700

    fix

--- docs/app.js
+++ docs/app.js
@@ -4,6 +4,12 @@ document.getElementById('cat1').addEventListener('click', function(event){
   document.getElementById('cat-popup3').style.display = 'none';
 })
 
+document.getElementById('cat1').addEventListener('mouseleave', function(){
+  setTimeout(() => {
+    document.getElementById('cat-popup1').style.display = 'none';
+  }, 900)
+})
+
 
 document.getElementById('cat2').addEventListener('click', function(event){
   document.getElementById('cat-popup2').style.display = 'block';
@@ -12,8 +18,20 @@ document.getElementById('cat2').addEventListener('click', function(event){
 })
 
 
+document.getElementById('cat2').addEventListener('mouseleave', function(){
+  setTimeout(() => {
+    document.getElementById('cat-popup2').style.display = 'none';
+  }, 900)
+})
+
 document.getElementById('cat3').addEventListener('click', function(event){
   document.getElementById('cat-popup3').style.display = 'block';
   document.getElementById('cat-popup1').style.display = 'none';
   document.getElementById('cat-popup2').style.display = 'none';
+})
+
+document.getElementById('cat3').addEventListener('mouseleave', function(){
+  setTimeout(() => {
+    document.getElementById('cat-popup3').style.display = 'none';
+  }, 900)
 })
